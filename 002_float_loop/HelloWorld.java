e989d350a6baff650bf861ec5ff68bc37c56bad2
PVoLan <pvolan@mail.ru>
2021-03-18 11:45:05 +0300

    Sample 2

---
+++ 002_float_loop/HelloWorld.java
@@ -0,0 +1,45 @@
+
+
+class HelloWorld {
+
+  public static void main(String[] args) {
+    
+
+
+    ///////////////////////////////////////////////////
+    //Ignore everything above this line
+
+    
+
+    System.out.println("Sample 1 -------------");
+
+    float f = 0;
+
+    for (int i = 0; i < 1000; i++) {
+        f += 11.11f;
+        System.out.println("Iteration " + i + " f = " + f);
+    }
+
+    System.out.println("Result " + f);
+
+
+
+
+    System.out.println();
+    System.out.println();
+    System.out.println("Sample 2 -------------");
+
+    float a = 1.345f;
+    float b = 2.777f;
+    float c = a + b;
+    System.out.println("c = " + c);
+    System.out.println("c == (4.122f) = " + (c == (4.122f)));
+
+
+
+    //Ignore everything below this line
+    ///////////////////////////////////////////////////
+
+
+    }
+}
