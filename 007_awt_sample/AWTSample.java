f4a3ce433dd69cb984808b09ad52b91d749ae51f
PVoLan <pvolan@mail.ru>
2021-05-20 20:53:56 +0300

    Some style added

--- 007_awt_sample/AWTSample.java
+++ 007_awt_sample/AWTSample.java
@@ -7,18 +7,20 @@ import java.awt.event.WindowEvent;
 public class AWTSample {
 
     public static void main(String[] args) {
-        Frame f=new Frame("Hello World example of awt application");
-        Label label1=new Label("Hello World", Label.CENTER);
+
+        final Frame f = new Frame("Hello World example of awt application");
+        f.setSize(300,100);
+
+        Label label1 = new Label("Hello World", Label.CENTER);
         f.add(label1);
 
-        f.setSize(300,100);
-        f.setVisible(true);
         f.addWindowListener(new WindowAdapter() {
             public void windowClosing(WindowEvent event) {
                 f.dispose();
             }
         });
 
+        f.setVisible(true);
     }
 
 }
